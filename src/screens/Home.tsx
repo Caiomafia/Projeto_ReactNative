41c79d2531c92ba1c68e9908ff6b5926f0d9e0e8
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2024-03-04 14:42:33 -0300

    Commit ajuste

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { func } from "prop-types";
+import React from "react";
+import { Text , View } from "react-native";
+
+
+export  function Home(){
+    return (
+     <View>
+        <Text className=" text-center">Hello </Text>
+     </View>
+    );
+}
+
